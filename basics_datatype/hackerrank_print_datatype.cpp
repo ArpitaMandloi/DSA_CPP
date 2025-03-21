b21da63ae2489d3b0214ff87e0cb0116ff9643d9
ArpitaMandloi <arpitamandloi369@gmail.com>
2025-03-21 20:47:03 +0530

    Create hackerrank_print_datatype.cpp

---
+++ basics_datatype/hackerrank_print_datatype.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    long l;
+    char c ;
+    float f ;
+    double d;
+    
+  
+      cin>>a>>l>>c>>f>>d;
+    cout<<a<<endl;
+    cout<<l<<endl;
+    cout<<c<<endl;
+   cout << fixed << setprecision(3) << f << endl;
+    cout << fixed << setprecision(9) << d << endl;
+    return 0;
+}
