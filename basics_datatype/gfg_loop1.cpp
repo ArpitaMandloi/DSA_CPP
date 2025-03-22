59caa528354cf6133c065c6aa463fe7de5baf2d6
ArpitaMandloi <arpitamandloi369@gmail.com>
2025-03-22 13:12:33 +0530

    Create gfg_loop1.cpp

---
+++ basics_datatype/gfg_loop1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public void calculateMultiples(int n) {
+        
+       
+        int i = 1;
+        while( i <= 10){
+            n * i;
+               --i;
+        }
+    }
+};
